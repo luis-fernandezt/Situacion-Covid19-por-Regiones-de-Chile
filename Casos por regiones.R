ad2bd5b26419f532c849a16733c2c1ba4765a90b
luis-fernandezt <luisguillermo.fernandez@outlook.com>
2020-09-16 12:38:56 -0300

    Actualizado 14 sep 2020

--- Casos por regiones.R
+++ Casos por regiones.R
@@ -25,7 +25,7 @@ regiones$Region <- c("Arica y Parinacota",   "Tarapacá",   "Antofagasta",   "Mag
                      "Maule",   "O'Higgins")
 
 # Paso 2 - cargamos reporte diario desde Minciencia (ACTUALIZAR FECHA)
-producto4 <- read_csv("https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto4/2020-09-13-CasosConfirmados-totalRegional.csv")
+producto4 <- read_csv("https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto4/2020-09-14-CasosConfirmados-totalRegional.csv")
 producto4 <- as.data.frame(producto4)
 
 names(producto4) <- c("Region", "Casos_totales_acumulados", "Casos_nuevos_totales", "Casos_nuevos_con_sintomas",
@@ -143,14 +143,11 @@ sft_tasa_fallec$tasa_fallec_100mil_qt <- cut(sft_tasa_fallec$tasa_fallec_100mil,
                                              include.lowest = T)
 
 # limpiamos la BD
-rm(Region)
 rm(Fallecidos_totales)
 rm(Casos_activos_confirmados)
 rm(Poblacion)
 rm(tasa_fallec_100mil)
 rm(tasa_activ_100mil)
-rm(producto4)
-rm(shp)
 rm(mps_casos)
 rm(mps_tasa100mil)
 rm(mps_tasa_fallec)
@@ -265,7 +262,7 @@ ggx <- ggarrange(gg1, gg2, gg3 + rremove("x.text"), #lento
                  ncol = 3, nrow = 1)
 
 ggx1 <- annotate_figure(ggx,
-                        top = text_grob("Situación covid19 por regiones\n13 de septiembre de 2020", 
+                        top = text_grob("Situación covid19 por regiones\n14 de septiembre de 2020", 
                                         color = "black", face = "bold", size = 16),
                         bottom = text_grob("Fuente: Minsal, github.com/MinCiencia", 
                                            color = "grey",
